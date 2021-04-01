cb7378cd1c8670cd37ed1466c6cbe89501174db8
Alejandro <jeffersonalejandro27@gmail.com>
2021-04-01 13:04:39 -0500

    ultimos cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router(); //Aqui definimos las rutas de nuestro servidor
 const app = express()
+const path = require('path'); 
 
- router.get( "/eje" , ( req, res ) => {
-    res.render('index.html')
+ router.get( "/" , ( req, res ) => {
+  res.sendFile('index.html', {root :  path.join(__dirname , '../public')}); //Cuando no se utliza ningun motor de plantilla lo hacemos asi
 })
+
+ router.get( "/about-us" , ( req, res ) => {
+  res.sendFile('about-us.html', {root :  path.join(__dirname , '../public/views')}); //Cuando no se utliza ningun motor de plantilla lo hacemos asi
+})
+
+ router.get( "/contacts" , ( req, res ) => {
+  res.sendFile('contacts.html', {root :  path.join(__dirname , '../public/views')}); //Cuando no se utliza ningun motor de plantilla lo hacemos asi
+})
+
+ router.get( "/blog" , ( req, res ) => {
+  res.sendFile('blog.html', {root :  path.join(__dirname , '../public/views')}); //Cuando no se utliza ningun motor de plantilla lo hacemos asi
+})
+
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express(); 
 const path = require('path'); 
+var bodyParser = require('body-parser');
 
-
-app.use(express.static(path.resolve(__dirname, './public')));
 app.use(require('./routes/index.js'));
-app.set('views', path.resolve(__dirname , 'public/views'));
-app.set('view engine', 'html');
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(require("../src/routes/index"))
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+// app.get("/test" , ( req , res ) => {
+  // res.render("contacts.html")
+  // res.sendFile('test.html', {root : __dirname + '/public/views'}); //Cuando no se utliza ningun motor de plantilla lo hacemos asi
+// })
+
+
 
 
 // app.use(express.static(path.join(__dirname, 'public')));
