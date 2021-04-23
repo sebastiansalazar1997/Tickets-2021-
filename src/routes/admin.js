8cbfd983c1e47dfc688cc926d05e790c4a744e17
Alejandro-Laptop <jeffersonalejandro@27gmail.com>
2021-04-23 11:55:01 -0500

    Login de admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,8 +2,30 @@ const express = require("express");
 const app = express();
 const path = require('path')
 
+app.get("/admin/login" , ( req, res ) => {
+  res.sendFile('loginAdmin.html', {root :  path.join(__dirname , '../public/views')});
+});
+
+app.post("/admin/login" , ( req, res ) => {
+
+  if( req.body.email == "1" && req.body.password == "1" ){
+    res.send('Pd445fDa54')
+  }else{
+    res.send(false)
+  }
+  
+});
+
+
+
 app.get("/admin" , ( req, res ) => {
-  res.sendFile('admin.html', {root :  path.join(__dirname , '../public/views')});
+  let token = req.query.token;
+  if( token == 'Pd445fDa54' ){
+    res.sendFile('admin.html', {root :  path.join(__dirname , '../public/views')});
+  }else{
+    res.redirect('/admin/login')
+  }
+    
 });
 
 module.exports = app;
